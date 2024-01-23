8b37417821539e1d36404de1502004188c2d96bc
서지원 <sjs3326@naver.com>
2024-01-24 01:00:02 +0900

    feat: 회원가입 구현(진행중)
    Former-commit-id: ff3c851476598e67acefca36dffeab725deacf61

---
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4d9d5f3a422cd3619022eebe2ca3effb5f360ca52f6bf57c7dc224cf88cf7aa0
+size 868
--- backend/src/main/java/com/ssafy/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba115777b56869e479ed060379c91b58eff7d002ac0ff1309f7c8bc37fe69062
-size 807
+oid sha256:e218fd3543a337f01779694f57d17942412c78bfbdf80cc3df4537f31fe7d6fd
+size 1352
