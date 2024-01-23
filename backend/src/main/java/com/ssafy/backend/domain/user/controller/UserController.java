361d0b91adc1ee2e48c461b74763b48392e87715
서지원 <sjs3326@naver.com>
2024-01-23 17:45:26 +0900

    feat: User 초기 세팅
    Former-commit-id: 4941a4e5175e8037ec61da4415d844afdbe030c9

---
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b8e8b3744138d5c01da93171cc565ac7a5f6af98865488cfbd20954e35c0f337
+size 517
---
+++ backend/src/main/java/com/ssafy/backend/domain/user/entity/User.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ba115777b56869e479ed060379c91b58eff7d002ac0ff1309f7c8bc37fe69062
+size 807
