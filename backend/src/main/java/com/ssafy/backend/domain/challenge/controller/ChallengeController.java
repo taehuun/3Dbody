40b376852057c057e30826df95185cb5b7c53dca
박형규 <gudrb4869@naver.com>
2024-01-23 17:54:09 +0900

    feat: Challenge 초기 세팅
    Former-commit-id: 696a7fa91faa2a9435701afa83c666c3a015c3f3

---
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/controller/ChallengeController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:250be5954e02172e708c5515e44e1ea01229332d41e0ba0c8a0a0c6c79b35b56
+size 536
---
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/entity/Challenge.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6ab8cca8de8378fd2cbf5f332b0cdd003cc0ccc937d3766c6a8e7bd64d0e3f8
+size 1340
