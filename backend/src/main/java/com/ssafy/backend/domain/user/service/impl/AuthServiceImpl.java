793d51c4a58fada8655cd353d51501898b9ee412
서지원 <sjs3326@naver.com>
2024-01-24 17:44:19 +0900

    feat: 회원가입 구현(진행 중) DB 저장, 닉네임 랜덤 할당
    Former-commit-id: 0d839791f965dcea9334f194e6fcf46b1aae0668

--- backend/src/main/java/com/ssafy/backend/domain/user/service/impl/AuthServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/service/impl/AuthServiceImpl.java
@@ -1,13 +1,3 @@
-package com.ssafy.backend.domain.user.service.impl;
-
-import com.ssafy.backend.domain.user.entity.User;
-import com.ssafy.backend.domain.user.service.AuthService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthServiceImpl implements AuthService {
-    @Override
-    public void signup(User user) {
-        System.out.println(user.toString());
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd2106c1aca2a76649349c29a038f9830fdbd912a506873bd45be6e083d1fe89
+size 1189
