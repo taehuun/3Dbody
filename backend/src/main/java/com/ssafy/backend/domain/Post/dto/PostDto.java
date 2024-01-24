a8e198d87f0ea7e4abf9603ec65bca1961165bb5
김태훈 <xogns5@naver.com>
2024-01-24 22:19:25 +0900

    작성,조회추가
    Former-commit-id: f6967dedf92877b4fd91ef679f506fee87b9f480

--- backend/src/main/java/com/ssafy/backend/domain/Post/dto/PostDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/Post/dto/PostDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:198f1596cc75626d49c71deb397dc03ea1955f22cf9f6f72c075ebd49134460d
-size 1359
+oid sha256:c99e989cd37142c6486dca69666e73eefdff3c0a8fa74f80189a0e9e9f3cd8b6
+size 1324
--- backend/src/main/java/com/ssafy/backend/domain/Post/service/PostServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/Post/service/PostServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4190dd3835f70ffb300bcf64af5190ae7e2ed17bf159d2365db2cf6874e4c588
-size 2025
+oid sha256:d4f76bf55fc1429b0ef530f776f37b7443cfd9ebcca8fa13d5d3fe243339fa02
+size 2115
