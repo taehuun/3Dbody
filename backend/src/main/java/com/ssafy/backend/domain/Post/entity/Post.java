2ff7b3536cdd816eae972aa929c49029233aa906
김태훈 <xogns5@naver.com>
2024-01-24 22:19:25 +0900

    작성,조회추가

--- backend/src/main/java/com/ssafy/backend/domain/Post/entity/Post.java
+++ backend/src/main/java/com/ssafy/backend/domain/Post/entity/Post.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15bf9df5079a0251d0bd5e61196c9e104cf119bc372aa8332c1cab02d39409db
-size 849
+oid sha256:7101428175115dbcee7ab82c98eacfd9b03ee6bbc15d9f486ea2a5f751ec98cb
+size 1178
