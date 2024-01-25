f067db2ac1f6d3b83cf4794940613faa09cf9d64
seonggwon <rkdtjdrnjs11@naver.com>
2024-01-25 14:41:50 +0900

    chore/tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acb24feb3540b5ee687f5a65bdbb81fe3644aa19da89256bc24b18d8ee010410
-size 235
+oid sha256:467fe1451c3b1d11f4109910066a63d39df06c4d247ae70fc4b3437655097b6a
+size 323
