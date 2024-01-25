242bc8a0b72fb8693da3e2a5825d363159c2c519
서지원 <sjs3326@naver.com>
2024-01-25 13:21:29 +0900

    feat: 로그아웃 구현
    Former-commit-id: 0532af7fc2e64e9df32c8c29244d46b50f5d427d

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55d76b2102bfead386ff134d1b1f8918092a70df981654ca460bf95c46558521
-size 2980
+oid sha256:0bd70e3c4cdf6aec923792706e610e5c7474345d733f55a43068c654b9efac04
+size 3463
