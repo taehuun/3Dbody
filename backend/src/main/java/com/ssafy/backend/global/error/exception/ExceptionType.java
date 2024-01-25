279293b5cd368af4bcaca410b850cf47ff7c17a8
서지원 <sjs3326@naver.com>
2024-01-25 16:21:08 +0900

    fix: ExceptionType 수정

--- backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
+++ backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:879d4945764c1b5ca987ac9ce19dde8b95d25963f5ea5e50bca9885adfab9315
+oid sha256:fd41b6c2ec77d9d057ba3255fab4077e924ba3222415f2157c80601f509c48ec
 size 1171
