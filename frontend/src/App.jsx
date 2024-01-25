3caa6c26f37fba5e313976e3cc6ce0e9de3da2be
이승호 <kials1@naver.com>
2024-01-25 15:30:27 +0900

    pwa 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:421a333d4679d0e3cd43aedcef60be38e2c08e84cc050197d6658b27e3feb102
-size 96
+oid sha256:0e0ef820b035b2554c04fbb70841998c51690c26772b734c8ba6a862e7d53f15
+size 108
