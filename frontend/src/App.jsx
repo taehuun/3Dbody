1ef3e0151f30f1f64443d3625e141b73f6570702
김민욱 <minwookkim@naver.com>
2024-01-25 14:55:46 +0900

    clean

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f46e5255340544a2831122c7717369439f30e722ebab8f01cb46f5f1a7e9652
-size 383
+oid sha256:421a333d4679d0e3cd43aedcef60be38e2c08e84cc050197d6658b27e3feb102
+size 96
