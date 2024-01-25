13d2c0b607556bf185f9e272192d4f2ba01bfa05
이승호 <kials1@naver.com>
2024-01-25 17:49:16 +0900

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e0ef820b035b2554c04fbb70841998c51690c26772b734c8ba6a862e7d53f15
-size 108
+oid sha256:f09b5f271a8da4ada3683b5c6cf2c8751eae2b0e48540ae01a83eed53c4c7f63
+size 181
