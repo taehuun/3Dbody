b12addff0779fb702cbc82f255a5051529e0565f
김태훈 <xogns5@naver.com>
2024-01-26 22:22:10 +0900

    Chore:도커파일,젠킨스파일추가
    Former-commit-id: 24c640da725289d20b63a349081443e3d2af7655

--- backend/src/main/java/com/ssafy/backend/domain/Post/dto/PostDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/Post/dto/PostDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c99e989cd37142c6486dca69666e73eefdff3c0a8fa74f80189a0e9e9f3cd8b6
-size 1324
+oid sha256:a69b8807d00c6bd57dda67a9c83030bba6653d4c9a751f25d8821640a954b6f8
+size 1353
