16bfafc0876f4be4a7094d4eba322f8523d13a9c
김민욱 <minwookkim@naver.com>
2024-01-26 09:05:01 +0900

    Style : 세미콜론
    Former-commit-id: 0f5836cb2e60a98d269263b0dd652dbd47f5e1f2

--- frontend/src/common/ToggleTap.jsx
+++ frontend/src/common/ToggleTap.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:320375b49d89c4abc95f471f79dc51bd3560a74aed3f5796140965c2a26e1159
-size 859
+oid sha256:209598121ecf8565ffa5db860c845c40fc7aade3a84ba0395804e471e71d59bf
+size 872
