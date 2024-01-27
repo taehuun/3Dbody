59b2c016b00812dbcd6527ec2b22b77899b30c7e
서지원 <sjs3326@naver.com>
2024-01-28 00:35:42 +0900

    feat: 이메일, 닉네임 중복 ExceptionType 추가

--- backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
+++ backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd41b6c2ec77d9d057ba3255fab4077e924ba3222415f2157c80601f509c48ec
-size 1171
+oid sha256:9ba8d05cb6cd7fe4de40a01af9dce3b3c5321a4892e16cf3ced2ff2c5927a4d0
+size 1355
