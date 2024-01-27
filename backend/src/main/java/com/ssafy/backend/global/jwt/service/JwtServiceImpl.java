98d782b3fd45f88bcabb5a154ff93f59861b6b97
김태훈 <xogns5@naver.com>
2024-01-27 16:34:29 +0900

    fix:jwt구조수정
    Former-commit-id: 7b6ed84f14cc4d061f54f35a0cacb3cace0b717c

--- backend/src/main/java/com/ssafy/backend/global/jwt/service/JwtServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/global/jwt/service/JwtServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f952bcf4843a4315d8029f096b9b84dfe290cc74cbb7516efa8d486d5b0dcbd
-size 5726
+oid sha256:0e9bbc315e39951ca79290f802ea43c02ee763d2efcddc2ad558b6160251418f
+size 5914
