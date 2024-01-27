b9add1de3d05ceaff9d912ff149bc18a2de25090
김태훈 <xogns5@naver.com>
2024-01-27 18:38:04 +0900

    db서버연결추가

--- backend/src/main/java/com/ssafy/backend/domain/challenge/dto/ChallengeListResponseDto.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ad0d1f9cf208722332a4079ded40d82a1341d91fd5448fd31dd3888abfa72129
-size 927
--- backend/src/main/java/com/ssafy/backend/domain/challenge/repository/UserChallengeCustomRepository.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b6bb7078e2721cbc40f5dc33de438947b1cd5229a72e3e723a6c3dd4fbde01b8
-size 299
