750089e1e48619e7393129cd092bf0535ab3e250
@rkdtjdrnjs11 <rkdtjdrnjs11@naver.com>
2024-01-29 07:57:44 +0900

    feat: 간단한 화면구성만
    Former-commit-id: a73c213c25ca56f9e446cbea63dd01741494e4f4

--- frontend/src/components/mypage/Menus.jsx
+++ frontend/src/components/mypage/Menus.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cc50ef7db6056ec03ef05da0617789401394dc99d283c348b4ac7000123f082
-size 1530
+oid sha256:77419a8c16a22c2ca659f413ea25645c3b0fa0f0b4bc8935e44f94ab778e7795
+size 1539
--- frontend/src/components/mypage/Profile.jsx
+++ frontend/src/components/mypage/Profile.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51be76e3ba60c77af43fd2063b4cf04ee606704663573284e25927c25914ca5e
-size 699
+oid sha256:dee4a1b01afc5a8c7fc80d94b2b4cbf95c9e4b2472b92537049f263a31c552fc
+size 705
