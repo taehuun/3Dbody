69e4bd3062f597e896d43ca7f2438d3c436ec550
김태훈 <xogns5@naver.com>
2024-01-29 15:39:03 +0900

    feat:feat/notice내용변경
    Former-commit-id: 85d9816665a952d7a7b3b9d9244ba6062c03b9b2

--- backend/src/main/java/com/ssafy/backend/domain/Post/service/PostServiceImpl.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:01ccc79686887369c3817b8ba018db4db01199248c494a57f2c36ce9492576b5
-size 2137
---
+++ backend/src/main/java/com/ssafy/backend/domain/post/controller/NoticeController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d648613bfc55785ae10bd5c0bba91b53d58c78bc938127f4e0dfff206ba873a5
+size 847
