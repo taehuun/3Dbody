a07b784e354fd81628b725d72ccd657fe6e044da
김태훈 <xogns5@naver.com>
2024-01-29 21:39:00 +0900

    feat:list추가
    Former-commit-id: 135aaf2c4b960964b51cbeda3e70fbc6702f47cd

---
+++ backend/src/main/java/com/ssafy/backend/domain/post/dto/PostListDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2f2a65a23e7cf51ae915a698a04c108bdb286e2528adc045ec201563309789d1
+size 615
--- backend/src/main/java/com/ssafy/backend/domain/post/repository/PostRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/post/repository/PostRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8aafff5de3878fd71753887cc435851d71fda53c1603bd33d1d8b7156076be3
-size 390
+oid sha256:980db85b7e10a4d30ed542fbc182934d323e29757a4752a5d3323efb39759a5e
+size 1025
