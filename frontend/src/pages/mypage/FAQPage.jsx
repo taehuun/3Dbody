acef27c0b3ee882ba6b03607f6d9a084daac0484
seonggwon <rkdtjdrnjs11@naver.com>
2024-01-29 16:57:51 +0900

    feat: 기본 구성 완료 axios 연결
    Former-commit-id: 2a38ad9fe61b0af363c7d4712e393211113859e4

--- frontend/src/pages/mypage/FAQPage.jsx
+++ frontend/src/pages/mypage/FAQPage.jsx
@@ -1,9 +1,3 @@
-const FAQPage = () => {
-    return (
-        <div>
-          자주묻는질문
-        </div>
-    ) 
-}
-
-export default FAQPage
+version https://git-lfs.github.com/spec/v1
+oid sha256:2a3b3d44c319671390d8887687ae6bdf788f39d908839de179d90fc7cac9e062
+size 828
