f1c8625a9d798cfbb4e73aab359e2d2dcd65d5a6
박형규 <gudrb4869@naver.com>
2024-01-29 16:05:30 +0900

    fix: 챌린지 엔티티 제약조건 수정 user_id NULL에서 NOT NULL로 변경
    Former-commit-id: 2764c65031195ae77fc54cccd3554dd78f147352

--- backend/src/main/java/com/ssafy/backend/domain/challenge/entity/Challenge.java
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/entity/Challenge.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:120ff9f073ea5d765d3d66a372a9b23de75b9d50fa328efb0591a1cfab536106
-size 1553
+oid sha256:7769d892f903ca8bf6063bca73d527d380098dc86ed50dee87f1f1e67536d358
+size 1571
