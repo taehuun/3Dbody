4adff6a08e7de90b021eeb2f840e0d4c424fa909
박형규 <gudrb4869@naver.com>
2024-01-29 15:56:26 +0900

    feat: 챌린지 도메인 한줄설명 컬럼 추가 - 컬럼명 : summary - Entity, Dto, Service에 summary 관련 기능들 추가했음
    Former-commit-id: de00ed0a7f9cb3f5e82f6f1d3271f0f910bc303f

--- backend/src/main/java/com/ssafy/backend/domain/challenge/dto/ChallengeDetailResponseDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/dto/ChallengeDetailResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c23b445dbc11ffb0675118c6d9c9a68c989f94d7e3a4b9bdf29601dc71e8d22
-size 1564
+oid sha256:d1822d9980a7e7a6c7de6fc8bad7445fd2f5c7dcef8dad48b2a3fdc2e5ae95cc
+size 2072
--- backend/src/main/java/com/ssafy/backend/domain/challenge/entity/Challenge.java
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/entity/Challenge.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ea735422a6bdf458ba3dc367f586129465b3f33fcd0a20875505f234a205d15
-size 1361
+oid sha256:120ff9f073ea5d765d3d66a372a9b23de75b9d50fa328efb0591a1cfab536106
+size 1553
