c57dbf3615d0877812b431e2fe381dbc817c252a
seonggwon <rkdtjdrnjs11@naver.com>
2024-01-29 09:48:28 +0900

    feat: 페이지 이동
    Former-commit-id: 54c0b7dc54d9a0987348b7277a5839aa22d1c350

--- frontend/src/components/mypage/Profile.jsx
+++ frontend/src/components/mypage/Profile.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dee4a1b01afc5a8c7fc80d94b2b4cbf95c9e4b2472b92537049f263a31c552fc
-size 705
+oid sha256:7cd3734a2db28e695f7aa408a0a374cf21e8f343ae1c128f23f21d6c68276fa4
+size 764
