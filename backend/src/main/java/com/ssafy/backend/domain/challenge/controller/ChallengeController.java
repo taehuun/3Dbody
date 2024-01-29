9682d894651d481469701e28a8f61384b2427fc8
박형규 <gudrb4869@naver.com>
2024-01-29 17:14:15 +0900

    fix: 챌린지 컨트롤러 어노테이션 수정 @Controller를 @RestController로 수정 @RequiredArgsConstructor 추가
    Former-commit-id: 498d4d83017b5c99f09f753c6cdff34a18b369e8

--- backend/src/main/java/com/ssafy/backend/domain/challenge/controller/ChallengeController.java
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/controller/ChallengeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57eb01ebe64e22ea53a1b0aede39a80acdd58f5970967c9e065224ee153b2496
-size 4111
+oid sha256:8d4ebafd6242104f5ffaf01273c15e9e22720607c8a635b744f5ae3586e7fa94
+size 4054
