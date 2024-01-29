8a87ede01922b476686970789516d53eb31c1592
박형규 <gudrb4869@naver.com>
2024-01-29 14:21:49 +0900

    feat: 챌린지 정보 삭제 api 구현
    Former-commit-id: 17c502fe1bcc7ad48bb0caafad1664ec039b5bb6

--- backend/src/main/java/com/ssafy/backend/domain/challenge/controller/ChallengeController.java
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/controller/ChallengeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd4f42a28d445f1c534521c9e2cfb109f2c847343b0e70f6d455579358c41373
-size 3519
+oid sha256:57eb01ebe64e22ea53a1b0aede39a80acdd58f5970967c9e065224ee153b2496
+size 4111
