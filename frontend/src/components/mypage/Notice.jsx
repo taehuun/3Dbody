f896ecc44d1892ca63fe4b3a07731e8cb3c5a885
seonggwon <rkdtjdrnjs11@naver.com>
2024-01-29 17:19:01 +0900

    feat: 마이페이지 화면구성

--- frontend/src/components/mypage/Notice.jsx
+++ frontend/src/components/mypage/Notice.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1d4a8f97189a0db3593d79f50c795c7b441bdfd26626c14fdfa39c5487cfcbc
-size 317
+oid sha256:9e9156e8bc51dc80248ddef639bb240610a31f310acdd94ad8241cf05c9bf591
+size 361
