8136b15c65c9c4d5501ad46d20be62cb20c9b532
이승호 <kials1@naver.com>
2024-01-30 11:56:13 +0900

    Feat: 카렌다 운동관리 링크

--- frontend/src/pages/diary/DiaryPage.jsx
+++ frontend/src/pages/diary/DiaryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4adc81c183d4b80766903d6c0e9c47ba31d599e4586eca145c897780128f418a
-size 294
+oid sha256:f80ade3b388f523023f70af4431d590e55a527740c97ee42068bad88ba43f3e1
+size 750
---
+++ frontend/src/pages/diary/DiaryTrainingPage.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e3fd46d9b3fd0b191d2f036d47efb1172bf90609b34070e871a4307360f603a
+size 210
