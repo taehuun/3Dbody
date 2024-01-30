c0485df21f4cec6b82fe3041c6dd8c77adef20c5
김민욱 <minwookkim@naver.com>
2024-01-30 11:01:07 +0900

    Feat: 리코일 사용해서 토글바

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c024e5b564652cbe68c1c4a854cdbc9b170795a4925531d618e71b5242d9392
-size 399
+oid sha256:d549440f0d92255ac8e623a1c6c4cf947ae80e858bd86e33f937143d4c5046f0
+size 400
