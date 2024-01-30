e5cada1d5d011dab2f9cb783f9a2260370c5cd00
이승호 <kials1@naver.com>
2024-01-30 10:30:49 +0900

    Feat: 캘린더 선택날짜 표시

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:467fe1451c3b1d11f4109910066a63d39df06c4d247ae70fc4b3437655097b6a
-size 323
+oid sha256:0d3149c330d59bcc7e05c341a5609524286088fe20cf4ad5fcc228e20a79e2f1
+size 404
---
+++ frontend/src/recoil/diary/SelectedDateState.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d986e2b6d6fe1f2864dfb4a0d71c7e8d5042a7cbbe8ec56c7544a7d25aa442bd
+size 253
