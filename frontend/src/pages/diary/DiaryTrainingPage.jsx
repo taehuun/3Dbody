598afbd4e69a02c581c05080f4000b8ce709c775
이승호 <kials1@naver.com>
2024-01-30 15:59:04 +0900

    Feat: 캘린더 축소판
    Former-commit-id: 8f8ec4e8247f0fe3ad7bb83e00b4b86c24847a00

--- frontend/src/pages/diary/DiaryTrainingPage.jsx
+++ frontend/src/pages/diary/DiaryTrainingPage.jsx
@@ -1,11 +1,3 @@
-import PageTitle from "./../../components/common/PageTitle";
-
-const DiaryTrainingPage = () => {
-  return (
-    <>
-      <PageTitle pageTitle={"다이어리"} />
-    </>
-  )
-};
-
-export default DiaryTrainingPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:dedfd8dad7c7996c42c8a9721a07e76996f3ec6dff2568d119827fc87ecf18e1
+size 664
