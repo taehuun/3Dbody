43b944333b68d984f5db90a61a7f504106223143
서지원 <sjs3326@naver.com>
2024-01-31 00:10:41 +0900

    feat: 인바디 등록 구현(진행중)
    Former-commit-id: 34bbf765a02ec0138febb8d05dafb817a09eca50

---
+++ backend/src/main/java/com/ssafy/backend/domain/inbody/entity/Inbody.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca0e031633595cafe417ef4fbb61ebbe874e5be780d1b7339a33fda6a650a19e
+size 868
---
+++ backend/src/main/java/com/ssafy/backend/domain/inbody/service/imp/InbodyServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:507eb1f61ab73bdb33a34790f34a2b48b8f67d4ff629e1de7c4111c159544dbd
+size 1885
