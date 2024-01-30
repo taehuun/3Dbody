32813e170dc7fefc80349c0ac4813b88c703c769
seonggwon <rkdtjdrnjs11@naver.com>
2024-01-30 18:01:18 +0900

    비밀번호 변경
    Former-commit-id: a2fd208ac5f745848695f5407919fa14b708803e

--- frontend/src/pages/mypage/NoticePage.jsx
+++ frontend/src/pages/mypage/NoticePage.jsx
@@ -1,14 +1,3 @@
-import BackButton from "../../components/common/BackButton"
-import Notice from "../../components/mypage/Notice"
-
-
-const NoticePage = () => {
-  return (
-      <div>
-        <BackButton/>
-        <Notice/>
-      </div>
-  ) 
-}
-
-export default NoticePage
+version https://git-lfs.github.com/spec/v1
+oid sha256:02b8ad455245d3644354500500ebf4941b1fa5022c24858fc28cc6db515363e7
+size 829
---
+++ frontend/src/pages/mypage/PasswordChangePage.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0c9b0640922d5c74c419535e71dfb6c70a37f2200f956dac9554e7461e9ccda
+size 2634
