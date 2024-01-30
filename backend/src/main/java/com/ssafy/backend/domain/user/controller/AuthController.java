9cee7fc316bda4a3ac87f6adb2bacbbadde96b4b
서지원 <sjs3326@naver.com>
2024-01-30 13:22:40 +0900

    fix: 로그인 수정 Response에 토큰 추가
    Former-commit-id: 524e75e42fe97ba65727e7ce73e62b094e98fa90

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72f6fecd71dbb3364d0d6875fc9e07b4c1a5bd61b22c32c6e05d8123b746dc9e
-size 3852
+oid sha256:2f65db56bd6ecbf64729f516dc606e0493bb3ee43d9ec3e7ffa95ec5581ffdaa
+size 3424
