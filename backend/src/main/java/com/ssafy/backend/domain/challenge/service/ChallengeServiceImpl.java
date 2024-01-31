c667052126cf0524bb6a4771a46df7022183e49c
박형규 <gudrb4869@naver.com>
2024-01-31 11:39:38 +0900

    feat: 챌린지 참여 취소 api 구현
    Former-commit-id: 1dc34f7395719a84322a174d82a9fb22d0c8fe23

--- backend/src/main/java/com/ssafy/backend/domain/challenge/service/ChallengeServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/service/ChallengeServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c81c292fd537fa0ce922ead86ff9fbe556a00ceabe40ed8ae6c6579134a0cd31
-size 4717
+oid sha256:81a8e43b8325ab2b65d76ae24f68d63f2d24183608c8b1bee7fb0ebfe5d57e60
+size 5032
