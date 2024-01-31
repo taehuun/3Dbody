4eca76870f06b7ef0a1d9e390861ed11bab352df
박형규 <gudrb4869@naver.com>
2024-01-31 11:15:24 +0900

    refactor: 챌린지 커스텀 Repository 빈 주입 방식 변경 생성자 주입 위해  롬복 어노테이션 사용
    Former-commit-id: a64524d39d684738685add553d509fa43060aa62

--- backend/src/main/java/com/ssafy/backend/domain/challenge/repository/UserChallengeCustomRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/repository/UserChallengeCustomRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49440a970f3b086e9374e4f6fc7b632517e4e5393cf78a6eee4c87f27b01b6cc
-size 1053
+oid sha256:2cb9a9683eaf1578879c8c3e15d0588ae644ce17183a37ca032c4f8649d696e3
+size 1003
