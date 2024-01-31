be365fefbf7e6e346c44dc38af2f1c6da7515d20
김태훈 <xogns5@naver.com>
2024-01-31 10:10:57 +0900

    feat:음식목록
    Former-commit-id: 63fb2c43f97486a5a948f4cbe3bc5acce135dab1

--- backend/src/main/java/com/ssafy/backend/domain/user/dto/LoginRequestDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/dto/LoginRequestDto.java
@@ -1,13 +1,3 @@
-package com.ssafy.backend.domain.user.dto;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-public class LoginRequestDto {
-
-    private String email;
-    private String password;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:44d0c3f16e2269e7113b5603ccc0e23b035a5ce9110f0b957f7b799cc14be041
+size 217
