e314d099dec96ec9d57abb9e189e6caa28a92a75
서지원 <sjs3326@naver.com>
2024-01-31 14:25:58 +0900

    feat: 인바디 Exception 추가
    Former-commit-id: 0942edaf7acecdb4a67c32e6b8c29b8e4ce040ef

--- backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
+++ backend/src/main/java/com/ssafy/backend/global/error/exception/ExceptionType.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87268e70e1106fff53645dc43e7ebe7b88d23800d2ea19dd2a6bb21c90a77e5c
-size 1424
+oid sha256:b6d90e9ba4ca8b17ca5b96a965712bb76fcde89b08518774b77c101df393217f
+size 1512
