e1395962907ff1b9299ad6c2c5b045a3b1324887
김민욱 <minwookkim@naver.com>
2024-01-31 16:54:47 +0900

    Feat : 댓글 삭제 구현
    Former-commit-id: 51aa7ff28d735339513cc2781daf820fe7480fa8

--- frontend/src/components/challenge/ChallengeComment.jsx
+++ frontend/src/components/challenge/ChallengeComment.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85a9144038c8b57ea13078ac1f8346a08dcbd58d3e746d82f21522a0b96afc66
-size 1077
+oid sha256:a02cad2c59ac79c952a5585867d7a2eac11da7cbd5884d6a879a9db6dce00256
+size 1078
