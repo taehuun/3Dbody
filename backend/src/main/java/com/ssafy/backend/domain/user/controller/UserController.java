dc4daa15dc4d19a040982aa15fb13c07a888e16d
서지원 <sjs3326@naver.com>
2024-01-31 11:33:38 +0900

    fix: 비밀번호 변경 응답 상태 변경
    Former-commit-id: b17ce5b864087aa19d08e9f435c163dedcf23414

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03b1ae76a788434c1abd7544f5f63dc9808caa469c2a8704875c66bb092c9003
-size 5108
+oid sha256:c0ab2d702833030ef865d4868d591095c2368ef72ee3aaa3a46e9710b18b2d99
+size 5345
