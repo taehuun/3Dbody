d32ab183850e2b3c818f2a74e83fab5726ea2d88
서지원 <sjs3326@naver.com>
2024-02-01 10:48:46 +0900

    feat: 회원 정보 수정(진행중)
    Former-commit-id: c5748929e8d90517412611f3d064e560c64a0667

--- backend/src/main/java/com/ssafy/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bafd4a3b799690a6853d5c2ad303222fd3bd307d96b99c8507b934d9d19fa1c5
-size 1913
+oid sha256:a12abfb0838b2d6bfe16ea4d75f6beed4de26e997d88deb9ccd7268568f76024
+size 2431
