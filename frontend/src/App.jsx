55a3289a59a75fe54f6d05e0be431e95251b1efc
seonggwon <rkdtjdrnjs11@naver.com>
2024-02-01 18:00:59 +0900

    Feat: 모달중
    Former-commit-id: d233f6230b606c693524c1ca046109897c7693da

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed806edc5061ded8129ecfcc4fe14b2c0889a9078fe6e82d7962ff6bc26b5503
-size 495
+oid sha256:29584500908afb3530d2b4ea0d94834b780577b3d32ebe1a10dfbdf27a44354c
+size 527
--- frontend/src/recoil/modal/ModalState.jsx
+++ frontend/src/recoil/modal/ModalState.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39d665b2c0dbdf4d2fafee92f7d3f9adab8228187dfd5720ecb92619c02659f1
-size 125
+oid sha256:d72403544d8bdac42cc81443a781b05de451e8d40410c2fbfaad84cea68b694b
+size 229
