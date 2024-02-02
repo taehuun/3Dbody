14d225097177e5a5d2ee0570ba98fbb3b6954d48
seonggwon <rkdtjdrnjs11@naver.com>
2024-02-02 17:54:49 +0900

    2/2 마지막 커밋
    Former-commit-id: 875d19c187235039868edc642e25fa79ae03ea5a

--- frontend/src/components/mypage/Profile.jsx
+++ frontend/src/components/mypage/Profile.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff772734d32cc3644b9b21b1f0a0b62fd8df0c57060ce73d975c658131e6a189
-size 1661
+oid sha256:b058c5afc651449e2d3394d4e3aa040b6dcedf257f0fab4f6543f8cb243c1c2b
+size 1682
