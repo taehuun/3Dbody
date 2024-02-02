222cad3ea64d5eda800d0dd7750658c45a7bcc2b
서지원 <sjs3326@naver.com>
2024-02-02 10:30:17 +0900

    refactor: reissue 수정
    Former-commit-id: 024fcc995c0d1688e5c0047b48d997a2979c5115

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97380422b63c11e13eaeeb301f4f74ca2babc4798ca0bfe6a5eba502e778e195
-size 3497
+oid sha256:6580984e9ce22f962e361a72e215f1f622499ae4fdf20f1f3d413b3db5d518e2
+size 3095
