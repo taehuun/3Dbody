297223b8fa1ba82599e1574c8fd5287cd0774a9b
서지원 <sjs3326@naver.com>
2024-02-02 10:43:26 +0900

    refactor: AuthController 로그 추가
    Former-commit-id: c91f16b5b5919187ef241652d36c2779c2a370f2

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6580984e9ce22f962e361a72e215f1f622499ae4fdf20f1f3d413b3db5d518e2
-size 3095
+oid sha256:97b5f046e9985b097c4c06843162653ca26e71dec59644109edf86ef3bb49d93
+size 3169
