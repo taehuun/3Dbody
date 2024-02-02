0f833c7930edb8e789a3a965329791efb67aeb63
김태훈 <xogns5@naver.com>
2024-02-02 10:09:51 +0900

    test: 젠킨스테스트13
    Former-commit-id: 7f1f1ac8c03504d4a838046fc5cf5c53024b42fa

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c173cbec5368cd534c3054cadb9b205370825abd5a4fac73881b2cdde5b930c
+oid sha256:ba11e2d210dc101dd416d45e08ffc5bde66427b4b7c27ad408e44756ecee5a19
 size 3182
