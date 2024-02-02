8d1fa069b611dfa9b153f4680fb58a4b56b0e4a3
박형규 <gudrb4869@naver.com>
2024-02-02 12:15:45 +0900

    fix: 챌린지 정보 수정 api URI 수정
    Former-commit-id: f15179c703f0e675e040deca7c980d4ace5fce13

--- backend/src/main/java/com/ssafy/backend/domain/challenge/controller/ChallengeController.java
+++ backend/src/main/java/com/ssafy/backend/domain/challenge/controller/ChallengeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:499619dcdc852bbc9d6c76ae8abeb20aadb0ed61185bf299824cc85560502537
-size 5678
+oid sha256:de03db3f97b003ec9e749c7480c27206aaacad5d6003373bc8022a145201d8d2
+size 5803
