5c31f7872a6945e08bdbfd12004e0c88a63b2467
서지원 <sjs3326@naver.com>
2024-02-02 13:33:10 +0900

    refactor: Response 불필요한 import 제거
    Former-commit-id: 032b70bc46cf0070e5095c83446372e3b08068e8

--- backend/src/main/java/com/ssafy/backend/global/dto/Response.java
+++ backend/src/main/java/com/ssafy/backend/global/dto/Response.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16727dedff0898772903e463bdb40d22870a79ea88d24f719cf0fce2e29c375f
-size 2711
+oid sha256:a53902a1206654a477a40df2a188019e518ce05498199b32faa1a22c537eabd9
+size 2657
