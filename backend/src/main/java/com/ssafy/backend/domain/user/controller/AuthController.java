98d40bc0e44ae390d289fc0b4441aba0ea85a479
서지원 <sjs3326@naver.com>
2024-02-03 23:37:58 +0900

    refactor: auth exception 수정 및 적용
    Former-commit-id: 9d3b01046a92915e242090d3910ac1dbe44ce17e

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b382b5003f7b90041615c3cd1c74fea9e4304796b5037b95c65c2306e297641
-size 3263
+oid sha256:67f5d857db8f6417e4a50bb245ab2ad2c224ba528e359b32ab35ba9e4e75da8e
+size 3200
