dd3c56ce7ac8a2c12b6c7ca1dd4802bba716e986
서지원 <sjs3326@naver.com>
2024-02-03 23:35:04 +0900

    fix: 로그인 시 제공되는 user 정보 수정
    Former-commit-id: 1aeef3c3ff909372638a15e9d6e20a727b401f91

--- backend/src/main/java/com/ssafy/backend/domain/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52132cde70991f458e59d54dbc2eb0aa0dd52ab8b59e8423e17089cb70a9aa29
-size 1448
+oid sha256:f78fa937e380696b457f78fc64d9647f4c20b9d44426abfe794e6b700487badf
+size 1379
--- backend/src/main/java/com/ssafy/backend/domain/user/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/service/impl/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bb111fcf4396b704d7696275829e7e64ffaecc9ab138bdf15c0a3cc985429b9
-size 6431
+oid sha256:8494b65e77709d8d3579f217c931cc3545379c351489d48d52a7860a291dede9
+size 5784
