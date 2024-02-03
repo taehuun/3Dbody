d51a1d110784d2238252609f0cb5253b765acc32
서지원 <sjs3326@naver.com>
2024-02-03 23:51:49 +0900

    refactor: user exception 수정 및 적용
    Former-commit-id: 524a17c469dda423a4b1fef08105600d3a5bfe58

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd636a58ddc6ac72d7bd4cc45fb91e84530f8b2da06be049307ba37079b1f200
-size 7565
+oid sha256:0659b1daf97f6f05b6b3f802c74c319c263236e4069545b490cb04b7b8176773
+size 7717
