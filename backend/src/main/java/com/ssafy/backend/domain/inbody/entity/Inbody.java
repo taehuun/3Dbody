00e6f336096da461c1bf8821f897bd7c9439c9c6
서지원 <sjs3326@naver.com>
2024-02-04 20:56:03 +0900

    fix: 인바디 삭제 문제 해결 인바디 삭제 시 image 연관관계로 삭제안되는 문제 해결
    Former-commit-id: 70f0f7a0f04290074340135bdb37f3d12ed3d040

--- backend/src/main/java/com/ssafy/backend/domain/inbody/entity/Inbody.java
+++ backend/src/main/java/com/ssafy/backend/domain/inbody/entity/Inbody.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:591d3894ca25e995de6c1b3624bdf34e94d604f2443064f033be5614c12fe9ce
-size 2511
+oid sha256:d1a69f0b39294a2efe53193d241d97fc3def849d6cfbf79b8f8e6858f651f924
+size 2581
