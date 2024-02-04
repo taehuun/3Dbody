be1fa5fcb9d86da0b20e10e01673e38626c549da
김민욱 <minwookkim@naver.com>
2024-02-04 19:13:04 +0900

    Feat : 챌린지 수정 완료
    Former-commit-id: abed3725a3cac9844df9ea67054dd71ae01f3b41

--- frontend/src/pages/challenge/ChallengeDetailInfoPage.jsx
+++ frontend/src/pages/challenge/ChallengeDetailInfoPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20e5de3d8cc61f9f378de16cab9225519d57b64ac0dcd673ab3d16b48abd9109
-size 3715
+oid sha256:b5c26bed0ff4cead02029f52aaaa9318c76448add5597d1b9cc5fc415c17dc18
+size 3808
