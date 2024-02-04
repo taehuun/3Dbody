89d2d9bf9ff62a664b214aa4f03f366ff0fe99da
김태훈 <xogns5@naver.com>
2024-02-04 20:39:17 +0900

    fix: baseurl수정
    Former-commit-id: 12ffc58932a7176e588c190b2318ac6232ede362

--- frontend/src/recoil/common/BaseUrlState.jsx
+++ frontend/src/recoil/common/BaseUrlState.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddf871050d5cc243bcdf231522567cfbc11830bc2ca64aef9fa32471bbfee82a
-size 158
+oid sha256:cd11585b6cdf0e272d17a26bd624920c79e63b4e0147c10c6b589ff9a9fde2e6
+size 154
