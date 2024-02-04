34f6df481b6855bd0218d8369e7427ad6e28356a
서지원 <sjs3326@naver.com>
2024-02-04 21:30:03 +0900

    refactor: Inbody updateInboyImages 삭제
    Former-commit-id: b6c1d10505548a3c81847bfb29f94cd381eaa2e9

--- backend/src/main/java/com/ssafy/backend/domain/inbody/entity/Inbody.java
+++ backend/src/main/java/com/ssafy/backend/domain/inbody/entity/Inbody.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b34df54b3375b7123a1e24a2502723e8e04b80ab54297abe14c7746793cd563b
-size 2753
+oid sha256:dc2242e5b6034b1848097eb858de3a5a756b8bed5b6508c627db48b076d382e5
+size 2635
