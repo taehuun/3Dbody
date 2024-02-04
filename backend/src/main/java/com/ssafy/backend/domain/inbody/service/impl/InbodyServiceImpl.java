955d8bdd1faf6412f91b13d6ad2489f92a5b5e5c
서지원 <sjs3326@naver.com>
2024-02-04 21:29:23 +0900

    fix: 인바디 수정에 이미지 추가
    Former-commit-id: 200b0bfd57806818ceb2a45f5ee318305c073c5c

--- backend/src/main/java/com/ssafy/backend/domain/inbody/service/impl/InbodyServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/inbody/service/impl/InbodyServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a42fbcf4e34cb97147906362d29dd698303c1037fa9443717399bca4a53ce9e
-size 4130
+oid sha256:c18a97f3f96ad515d1a2e3a16e32607828c448c32bffe9b6890f6e0ebe7901e9
+size 4872
