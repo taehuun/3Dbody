b202992c132b7fe0f8fb16588b4765eae21dac62
김민욱 <minwookkim@naver.com>
2024-02-04 20:02:47 +0900

    Feat : 챌린지 기능 완료
    Former-commit-id: 24c54df925dc2ff7968c5a2855c16cefd90d819f

--- frontend/src/pages/challenge/CallengeCanParticipatePage.jsx
+++ frontend/src/pages/challenge/CallengeCanParticipatePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d9219f89f83381fd41a3b78c1f6f33d60efcfeeea7c338f3d692cf06e6a7b94
-size 1851
+oid sha256:ae5cef3f8057e2d11ad7df2ad55f70a4181143219cb8baef282e063b2c464e1d
+size 2163
