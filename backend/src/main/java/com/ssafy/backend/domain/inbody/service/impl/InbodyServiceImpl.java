0464913d2ed482913c83e48845cf0afbbb591b7a
서지원 <sjs3326@naver.com>
2024-02-04 21:51:23 +0900

    refactor: InbodyServiceImpl 불필요한 import 제거
    Former-commit-id: 876ac5e7cec69d478a036fae0e82ac1192cf8a83

--- backend/src/main/java/com/ssafy/backend/domain/inbody/service/impl/InbodyServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/inbody/service/impl/InbodyServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c18a97f3f96ad515d1a2e3a16e32607828c448c32bffe9b6890f6e0ebe7901e9
-size 4872
+oid sha256:bebcce27e73d165ca6896a9a236b86f59c54e71185c9b36239f8bfed9084e3a4
+size 4788
