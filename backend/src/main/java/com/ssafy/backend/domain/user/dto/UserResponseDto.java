4af12690cdda7f7d88e257f043ef564979e7110a
서지원 <sjs3326@naver.com>
2024-02-04 19:41:42 +0900

    fix: 로그인 시 제공되는 회원정보 수정 role 추가
    Former-commit-id: c6040744eece442f201174f0f289a7515cfb3488

--- backend/src/main/java/com/ssafy/backend/domain/user/dto/UserResponseDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/dto/UserResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7c6d80eeb411d1bdf430ecd9b6eaae51b988c0c0016f4072efffe598a80f580
-size 1109
+oid sha256:95f8108617d41fc072bc58468998fc23430a4979f6783867470f849dafedf58d
+size 1181
