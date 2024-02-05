3ec4765103a618b57819e8ec09c3b03e720848c7
박형규 <gudrb4869@naver.com>
2024-02-05 12:06:09 +0900

    remove: dto 삭제
    Former-commit-id: 2d4cca455fe535b6e97467768afbfbf0719ae1df

--- backend/src/main/java/com/ssafy/backend/domain/training/dto/UserTrainingRequestDto.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4fb07c1dbfe6844a46bac56cdcc22210d88906275b375a4d75bbe97a17cfbff3
-size 484
--- backend/src/main/java/com/ssafy/backend/domain/training/dto/UserTrainingRequestListDto.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3730a0c4f932ea17ee7d83db5e0f2f401af9d8059fe59c93588919530e723a7c
-size 486
