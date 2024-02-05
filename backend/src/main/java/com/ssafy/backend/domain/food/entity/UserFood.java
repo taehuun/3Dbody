a4370ed72d10167ace0f1420033ee091558e613a
김태훈 <xogns5@naver.com>
2024-02-05 14:43:00 +0900

    feat: 식사 추가
    Former-commit-id: c0d0154eecaf947809e02fd1ba9aaad4662990af

--- backend/src/main/java/com/ssafy/backend/domain/food/entity/UserFood.java
+++ backend/src/main/java/com/ssafy/backend/domain/food/entity/UserFood.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c474e29e58f914c5c1f0f1c1d5ecd15b8cc8c64ab5004440ac8094c6def29a5
-size 669
+oid sha256:b2c3a4437c60409a55e90c9fb5aec4811c5fc7c2efd42b31afc53e320e69f5fa
+size 756
--- backend/src/main/java/com/ssafy/backend/domain/food/service/FoodService.java
+++ backend/src/main/java/com/ssafy/backend/domain/food/service/FoodService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71d78ff92f17d1c920cbe60c7a067efd8b382fbfc599b0ac1b7a086cbed44092
-size 844
+oid sha256:d8a982b306e6f73c072f212aa884bf2b7be2ae16a751db673f0fc16cf5d7df6a
+size 892
