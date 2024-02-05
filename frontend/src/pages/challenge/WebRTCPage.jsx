5623bd6248cc0dd08c1d726adb8a3022fedf4028
김민욱 <minwookkim@naver.com>
2024-02-05 13:30:46 +0900

    Feat : RTC 진행 중
    Former-commit-id: 75edf0f33f745f5b3a01a5ac10aa13d10fc6aa52

--- frontend/src/pages/challenge/WebRTCPage.jsx
+++ frontend/src/pages/challenge/WebRTCPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c36a7cbfe0214d44dd2ae313be36c1140c1da5480b2c9b7d2baa77f3af55fe78
-size 7059
+oid sha256:a44f5294a6f2653dac7bb866080505f0269e4564b3814ee8bb2b49be5995aab9
+size 7131
