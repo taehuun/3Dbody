ed4e1197595252905f9e177b57d9d1a56c3d9f2e
김민욱 <minwookkim@naver.com>
2024-02-05 10:07:22 +0900

    Feat : 모바일에서 S3에 올리기 수정 중
    Former-commit-id: adcc43b05a2e1a8fc41240c880f211a5c2d108a5

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58d1efa55d69ee24cdfdb07f0a8e6c0945e7dc1610d61e1f487ccf3136286ec9
-size 1961
+oid sha256:81b57c5565bb90b292b8d9fa3db2e9e05185aec5428cd092826e0e94b1aa360c
+size 1887
