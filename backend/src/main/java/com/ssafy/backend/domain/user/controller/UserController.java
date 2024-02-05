56b639c42d9f8957da358732ea1e5649d83a10c9
서지원 <sjs3326@naver.com>
2024-02-05 13:25:08 +0900

    fix: 닉네임 중복 확인 401 문제 해결
    Former-commit-id: 6d061e272fbe9ce25283f6929826bbe073dfd5ec

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0659b1daf97f6f05b6b3f802c74c319c263236e4069545b490cb04b7b8176773
-size 7717
+oid sha256:604a6d2d800bc93f1f381b319d4bc4886dbca5e9a272d5caf863dea7ba53aaf1
+size 7778
