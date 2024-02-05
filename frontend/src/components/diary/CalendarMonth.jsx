4ad97b3a831b081c889ae2b6248ed0bbf522d698
이승호 <kials1@naver.com>
2024-02-05 17:57:16 +0900

    Feat: 음식 수정 머지
    Former-commit-id: 9b640bc6eb87a0266ad2a0c2997d1df7950d2abb

--- frontend/src/components/diary/CalendarMonth.jsx
+++ frontend/src/components/diary/CalendarMonth.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:854859124af56a9d961f689fe96be35876f541255646b507391b311dad24b902
-size 6626
+oid sha256:c19ecea922d4aaffb300cc39df7577ae814a32e6d1b6012422245e0757e06aab
+size 6625
