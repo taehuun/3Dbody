fb022e60c6e49c0950ec151defdb297e3ef9fa3c
김태훈 <xogns5@naver.com>
2024-02-05 17:48:15 +0900

    fix: 식단 userFoodCount기본값변경
    Former-commit-id: 68e89e7e29e4a6bfb9d87b86dae5f23f8bbb0bea

--- backend/src/main/java/com/ssafy/backend/domain/food/service/FoodServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/food/service/FoodServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a9e734bd71dcc1e18883eba9aceadb7d28789089f2723c715c1b72a74e69fd5
-size 4401
+oid sha256:b1b5359398dcfcb6bbd40ea44dcc0a5d194515f5db75114acc2c10a96786b1bc
+size 4492
