e64a0661598968dbe7e25bc3a5d010c019423dc0
박형규 <gudrb4869@naver.com>
2024-02-05 17:41:49 +0900

    feat: 세트 추가 api 구현
    Former-commit-id: 5edf011932c864eed7b6d694d56bd14d36716f10

---
+++ backend/src/main/java/com/ssafy/backend/domain/training/dto/SetResponseDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1fa634db8effb93cf5d03855c186f8c27ee8f85b3f52427a191e22b7f8d933e6
+size 691
--- backend/src/main/java/com/ssafy/backend/domain/training/entity/UserTraining.java
+++ backend/src/main/java/com/ssafy/backend/domain/training/entity/UserTraining.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e81e8c814f0c2306f0bd7734d5d568d7b803b527907b5f4200341d297f30ea6
-size 1641
+oid sha256:f142a28916a424b31bf14c3e94ebe8f8b76de782576bf96dd5cb8bb3b3aef69c
+size 1553
