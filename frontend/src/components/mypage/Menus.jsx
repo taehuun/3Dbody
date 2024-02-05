731257c9aed6e40d96cac7765824e787c266c6d7
seonggwon <rkdtjdrnjs11@naver.com>
2024-02-05 17:58:59 +0900

    회원정보 대부분 완료
    Former-commit-id: e8ff45280dc79fd0b87fbdb7647b5820c6f4746d

--- frontend/src/components/mypage/Menus.jsx
+++ frontend/src/components/mypage/Menus.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6c573f1c78434dd75fe0aa3607182a0c484fd3906e8de2eb0c86ed3b8f17fd5
-size 1575
+oid sha256:725a882ec358895c266f0ef8ce7b19398078bd4cf9e468a02b4bc279598c64ec
+size 1588
---
+++ frontend/src/components/mypage/UserDelete.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a61bf8a077fbbd6e7a4ed4a8c8bf31960311552bd133ae5e9ca8c5f9546b971c
+size 976
