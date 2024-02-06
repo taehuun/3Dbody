49beb76e54dde86d53868bb6b3db2b0e34ddc64f
김민욱 <minwookkim@naver.com>
2024-02-06 14:16:27 +0900

    Chore : env 수정
    Former-commit-id: f709659c87b221e89e334350c41e705c0b601a25

--- frontend/src/pages/challenge/WebRTC/components/buttons/MobileIconButton.jsx
+++ frontend/src/pages/challenge/WebRTC/components/buttons/MobileIconButton.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66c0713e6b3a06f06374643d078132db7c770de88b04ba2a92c908238e42465d
-size 4518
+oid sha256:8557759fa6e043e8bbac3c3032849f5ca525e51dbdbfd47aa2587d921892b9cd
+size 4519
