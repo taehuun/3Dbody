814bd0c26b9958f6b1d500569c7b4949997cfb92
김태훈 <xogns5@naver.com>
2024-02-06 23:07:50 +0900

    feat: 루틴상세,세트api추가
    Former-commit-id: c13dc48b4e7bceac211ea44b13f55143bdf2d244

--- backend/src/main/java/com/ssafy/backend/domain/routine/service/RoutineService.java
+++ backend/src/main/java/com/ssafy/backend/domain/routine/service/RoutineService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40603e3c65887bffa7384c15e764bdb2a5c61ab612750dd10192e3e8c9f7da64
-size 339
+oid sha256:58f22aaca383c502f6e63ed3cd0f6654d3f162f16d7b17cc1af799095a0eb480
+size 619
