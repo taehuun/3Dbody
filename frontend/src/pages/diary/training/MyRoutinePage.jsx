cca74650055d1caec73ee1c60fd3230769bab072
이승호 <kials1@naver.com>
2024-02-06 17:44:29 +0900

    Style: 대소문자 확인
    Former-commit-id: 26686c8d963c906f5c2c74bda969ee1d465dbe34

--- frontend/src/pages/diary/training/MyRoutinePage.jsx
+++ frontend/src/pages/diary/training/MyRoutinePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e427b41553a88a401f4089ab1b81a31ba800d9924f8d3fc8764bc696857174e
+oid sha256:3ab39f80122a313c8a6b3ea4fe69c6d0cc225dae7aaf903715e2186c58310ff5
 size 1694
