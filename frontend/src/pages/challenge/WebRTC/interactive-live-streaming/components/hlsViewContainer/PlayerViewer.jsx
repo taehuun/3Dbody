40efefaa5cce7cc9428466a2a1aaf812c24b29ff
김민욱 <minwookkim@naver.com>
2024-02-06 14:03:01 +0900

    Feat : WebRTC 잠정중단
    Former-commit-id: 130af179151d6fbaf05410d1c0eea3b4bafc60a7

--- frontend/src/pages/challenge/WebRTC/interactive-live-streaming/components/hlsViewContainer/PlayerViewer.jsx
+++ frontend/src/pages/challenge/WebRTC/interactive-live-streaming/components/hlsViewContainer/PlayerViewer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb1e7d0b69c046035cc05e050191dfdb0324502e3509c2872bb507a6a92e9138
-size 6433
+oid sha256:303ef9d4649718fbc95ca35780cfc83905c97a2a7375ddd2fd2aba02efd33d6b
+size 6610
