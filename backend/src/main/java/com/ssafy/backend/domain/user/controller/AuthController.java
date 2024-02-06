479a99f10ee3d1c7d7136b257c0c3cb4908a9ae7
서지원 <sjs3326@naver.com>
2024-02-06 10:19:50 +0900

    refactor: AuthController login 메시지 추가
    Former-commit-id: 0e49e2efce52712f5be657e12aa901c968e1a576

--- backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/AuthController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d924e30835aefcc351e6d895a637d48c9bb265c981922e7dc7c864272f2597c4
-size 3258
+oid sha256:c0e80bbc32418020c3ff8547ecce6909e2dfe24a2aa8661c82fd625c9ed10718
+size 3274
