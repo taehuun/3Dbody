02581663ad6a748e942827bdde4ef0ef87991b56
이승호 <kials1@naver.com>
2024-02-06 17:44:29 +0900

    Style: 대소문자 확인
    Former-commit-id: 40d37ccecbfd11245d07ecc8716befc57c736a3c

--- frontend/src/components/diary/training/TrainingDetailBox.jsx
+++ frontend/src/components/diary/training/TrainingDetailBox.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:855f352b1d341a59aea71c115ca9ad54472f3f82868fb428d5af625c88b9106b
+oid sha256:6d1b339a12f2ff46cb76053837c5a52500bd9a5b514599473228cdcc0f3be663
 size 3184
