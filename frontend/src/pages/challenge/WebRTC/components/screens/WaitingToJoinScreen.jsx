2681c45f631d7fcae054b86a74b38fbf633c43e2
김민욱 <minwookkim@naver.com>
2024-02-07 16:21:52 +0900

    Feat : RTC 기능구현 완료
    Former-commit-id: 99ccb2b569d4e545d2c2fdc309394b7dc5c3c308

--- frontend/src/pages/challenge/WebRTC/components/screens/WaitingToJoinScreen.jsx
+++ frontend/src/pages/challenge/WebRTC/components/screens/WaitingToJoinScreen.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb22bc33fb4c3f92b7ded578971044168b0e11d6543377fc10358f900b146de5
-size 2143
+oid sha256:561aef4a4debb690badddc822268a089308aa515b0348182b526bc32ac7bb728
+size 2201
