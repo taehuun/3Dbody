8749387d0c3fff12db4ad15722fcb6b97e54c406
박형규 <gudrb4869@naver.com>
2024-02-07 11:23:48 +0900

    fix : 세트 삭제 api 수정
    Former-commit-id: 0fa1ffec2a0e1e4437033b951d335d16df5e2cff

--- backend/src/main/java/com/ssafy/backend/domain/training/controller/TrainingController.java
+++ backend/src/main/java/com/ssafy/backend/domain/training/controller/TrainingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f4290325b040c3994e66399a55bfdf2f89d8cd20e789c27bd4f0f74f769a821
-size 6010
+oid sha256:e19f277aa5642866c2adea645531ccb70dcaeb2dad9f8f46754bffb70066212d
+size 6059
--- backend/src/main/java/com/ssafy/backend/domain/training/service/UserTrainingService.java
+++ backend/src/main/java/com/ssafy/backend/domain/training/service/UserTrainingService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb8eb280a3db15f7b517c9043df000abc1d00b26837c1406e104374516a199d1
-size 883
+oid sha256:31b2db9a06b5e1da32d2da84576cce0cda44d9c3f3053bdb614d716b6dd84574
+size 870
