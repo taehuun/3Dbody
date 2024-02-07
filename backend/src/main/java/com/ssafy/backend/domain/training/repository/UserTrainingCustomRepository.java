f83855027834b9cb22b09beb6a471477c1930535
박형규 <gudrb4869@naver.com>
2024-02-07 11:35:55 +0900

    fix: 세트 추가 api 수정
    Former-commit-id: aca4ec01124ee131e07b10c9d8ce75b29225563c

--- backend/src/main/java/com/ssafy/backend/domain/training/repository/UserTrainingCustomRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/training/repository/UserTrainingCustomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2722734fd3d74f37d7c77eb57edbb71f5c6ac2a6dcdaf47745413c224fac6fe7
-size 711
+oid sha256:fc5ce277c19fb7bec612aac289c42b6bac89f08ab6793b1173e2361499e75e51
+size 671
--- backend/src/main/java/com/ssafy/backend/domain/training/service/UserTrainingService.java
+++ backend/src/main/java/com/ssafy/backend/domain/training/service/UserTrainingService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31b2db9a06b5e1da32d2da84576cce0cda44d9c3f3053bdb614d716b6dd84574
-size 870
+oid sha256:bf592f41a33020b6ff282ba4ea33e702f77a85a90f587ffec398636c427b90d4
+size 876
