f22ff93cb1143d5b947814bfb5efe7a07f7ec323
서지원 <sjs3326@naver.com>
2024-02-07 17:19:04 +0900

    feat: 3D 아바타 생성 파일 변환 및 저장 구현 txt 파일 -> obj 파일로 변환 후 s3, db에 저장
    Former-commit-id: f2f656abe2360c7ab0294f5617bb0371add0663d

--- backend/src/main/java/com/ssafy/backend/global/file/controller/FileController.java
+++ backend/src/main/java/com/ssafy/backend/global/file/controller/FileController.java
@@ -1,16 +1,3 @@
-package com.ssafy.backend.global.file.controller;
-
-import com.ssafy.backend.global.file.service.FileService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/file")
-@RequiredArgsConstructor
-public class FileController {
-
-    private final FileService fileService;
-
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a347035b9a9c10deea5061837bb98add09e4f6d413af59b5f6d072f9fbfa22ca
+size 1618
