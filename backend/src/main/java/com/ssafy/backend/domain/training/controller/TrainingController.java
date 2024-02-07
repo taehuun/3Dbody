df1132699c2daba8617f494c7e945344451c95cb
박형규 <gudrb4869@naver.com>
2024-02-07 10:29:44 +0900

    fix: 운동 추가 응답 데이터 수정
    Former-commit-id: 893c789908e0d878feadc75f74a3c09e0ef0fe7d

--- backend/src/main/java/com/ssafy/backend/domain/training/controller/TrainingController.java
+++ backend/src/main/java/com/ssafy/backend/domain/training/controller/TrainingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cd7aad9b05d455e9ac5c2cdd294e5ca3891a15068580fc9632a56717e196d30
-size 5578
+oid sha256:064775801759c61415d8d32746e32c2a4cace8da5a3950b0c381a31691753af6
+size 5664
