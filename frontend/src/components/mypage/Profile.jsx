5841f43af4f960e978709c197341cdbc36a5029f
seungho <kials1@naver.com>
2024-02-12 02:53:11 +0900

    Fix: pwa, aws 수정
    Former-commit-id: 2523e8e16e30b66191baef13275e04c6470e06b9

--- frontend/src/components/mypage/Profile.jsx
+++ frontend/src/components/mypage/Profile.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d2862b2b09d365edf13a0068d13f5f8eaab541d636e04952e40485ed0f73f6b
-size 4111
+oid sha256:a698d22e2b78277da30062c7c7e09c0ee754834c14f78fea9d2a8ea174b0b0d4
+size 4037
