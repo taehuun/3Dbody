dba7e684e2a2b20a069019bbfac74accac2ac0ed
김태훈 <xogns5@naver.com>
2024-02-11 21:30:04 +0900

    feat: 루틴불러오기 추가
    Former-commit-id: 6704af2a58760f3703d7663604d85592163db1ed

--- backend/src/main/java/com/ssafy/backend/domain/training/dto/SetDto.java
+++ backend/src/main/java/com/ssafy/backend/domain/training/dto/SetDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e0b6a9b3ad44c7e3ba3e272e57683227c8b2eed2244124c899b50a6de0296e8
-size 1037
+oid sha256:d3c55041e73d9cfd6f7d579ce2ab1303af608128f237a26a1c13ce30febefd7b
+size 1045
