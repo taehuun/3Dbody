5d19d42a8ac72c746bea4b939e88511f35fa8a1d
김민욱 <minwookkim@naver.com>
2024-02-13 08:47:42 +0900

    Design : callenge CSS 수정
    Former-commit-id: 51858e2d2aa3f3fb73ffc6ddea08e83e5b5b784f

--- frontend/src/pages/challenge/ChallengeRegistrationPage.jsx
+++ frontend/src/pages/challenge/ChallengeRegistrationPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ea4368e1415823f834ee9eb0c54ddfb7b3d4b081ec11af776f28f2b7a6c94d8
+oid sha256:c7ea400a0605eaa2044cd2dad10fe366b3a8b57ed2fccbf76093f0ac0b4bb3cb
 size 8615
