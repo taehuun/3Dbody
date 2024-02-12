f5ce5da325607ff7ce8c196185c60a6869f64c81
seungho <kials1@naver.com>
2024-02-12 11:44:24 +0900

    Fix: 패스워드 autocomplete=on
    Former-commit-id: fbe8a977a4e47d8afc7d5bbc788173ee5f623d5f

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c65ef393b46b25b99780b5d287043444bb06255812cf53e7373de8096b7f81e5
-size 2382
+oid sha256:426fd955b9b9d709d70f218eca19553c5f0c17b36d50caa44f5fe035e805878c
+size 2400
