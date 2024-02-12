c50df6c2d95d0b65b12574bd007ae713693065e5
김민욱 <minwookkim@naver.com>
2024-02-12 15:32:56 +0900

    Design: challenge CSS 수정
    Former-commit-id: 4d4e1431ab9ad031fc28595bcd19e5bf31ca2eee

--- frontend/src/pages/challenge/ChallengeRegistrationPage.jsx
+++ frontend/src/pages/challenge/ChallengeRegistrationPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8316e57823821221e15998cfb87dd497a2c75c2607ccea96b6b98fb17f9fe9ea
-size 8559
+oid sha256:e2fd5fa821c35a53a1097de43223184355009561fed3ebefba745f4f9142fb80
+size 8577
