e9fde83f5a1c6d16e08c430f5c165a1d3ba41925
이승호 <kials1@naver.com>
2024-02-13 17:57:51 +0900

    Fix: 모델용 suspense 추가
    Former-commit-id: bb7e831c8420132e148c8067ce300e66506d8d66

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,3 @@
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./router/Router.jsx";
-import "./index.css";
-import { RecoilRoot } from "recoil";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RecoilRoot>
-    <RouterProvider router={router} fallbackElement={"로딩중!!!!!"} />
-  </RecoilRoot>
-);
+version https://git-lfs.github.com/spec/v1
+oid sha256:98a5467de38eb64a17b18effaca76057790c49368de6abaf9726cca46c6d980e
+size 430
