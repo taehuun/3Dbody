f8564f133e3dcd83de39aaf693f7684cb442d0a2
김민욱 <minwookkim@naver.com>
2024-02-13 11:08:32 +0900

    Design: challenge CSS 수정
    Former-commit-id: 613ece8d2a976337511b370de0e917668ac756aa

--- frontend/src/components/mypage/Profile.jsx
+++ frontend/src/components/mypage/Profile.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd84c7524ee00fab778f3e8574cb50ccaae6bca0a8f135cf6f61e440ef4d5732
-size 4135
+oid sha256:c16daa8bb875c01eda28ea808cc059d57a934427ebf1c157b6de27060fdec606
+size 4248
