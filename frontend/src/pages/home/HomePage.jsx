cb0bee1e082b4e07e8847d2b25dc7a6ada751836
이승호 <kials1@naver.com>
2024-02-13 17:57:51 +0900

    Fix: 모델용 suspense 추가
    Former-commit-id: a1c34c6c500dd0681030dfc64b100a142f774cd6

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cffb2b78fa8510781a4015c654b010ec3aee8f351ae656af75b7af6cedce174
-size 3180
+oid sha256:8a38e4ea41de21bdb487dccdc7ff9ac0f620c110f3926b1322e6a01e48d33398
+size 3116
