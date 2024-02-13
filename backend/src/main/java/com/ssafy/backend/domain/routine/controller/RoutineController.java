514e678a2d8a6bd1cf654ca08a3874870d549c3a
김태훈 <xogns5@naver.com>
2024-02-13 09:51:08 +0900

    fix: 젠킨스파일수정
    Former-commit-id: b1a84c6f76ddb570343b4a531de916fae5c27224

--- backend/src/main/java/com/ssafy/backend/domain/routine/controller/RoutineController.java
+++ backend/src/main/java/com/ssafy/backend/domain/routine/controller/RoutineController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e2f7a32f7bff6457d49f01176c4d5f59010626a1d1a29b6dd92b3cdd77584de
-size 7334
+oid sha256:fa8d52ee50214ab2325d6ea660410a25817527290e2f4ba23a3ed05c361f3d4e
+size 7359
