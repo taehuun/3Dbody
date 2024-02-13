b14deaf4679bc8cc9d3e3b75c94170fdba8b97b3
김민욱 <minwookkim@naver.com>
2024-02-13 17:06:35 +0900

    Feat: 3D 구현중
    Former-commit-id: e0eee697de2d6e34b3bf4e232a07c2f8696091c2

--- frontend/src/pages/challenge/WebRTC/components/screens/WaitingToJoinScreen.jsx
+++ frontend/src/pages/challenge/WebRTC/components/screens/WaitingToJoinScreen.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:607ea6a4ccec328e2d5ae4a917e1b1016f0f2b545a428197884db9e6ae027cc9
-size 1777
+oid sha256:300a9de15f4c7d2849603b82fb5d5dc57250551752b5c0ffeece6089b9e4e6d3
+size 1444
