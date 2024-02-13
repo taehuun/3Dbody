de3e7d7af58fe3eecbcc625ad3be066c9057d860
seonggwon <rkdtjdrnjs11@naver.com>
2024-02-13 14:10:32 +0900

    Feat: 모델 렌더링
    Former-commit-id: 9f438d35ed073766173a7799bd06cf7d56e5338e

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:262a2ac50d2832621496325272230847be2ba303e82653db1d0af69467de241c
-size 1706
+oid sha256:3e6ca6f7f51146fee577b3d3b3b61cb97d7ff42b1d51b73c3fe937fb661bc67d
+size 1509
