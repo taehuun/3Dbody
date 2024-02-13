3a76c70c776513bb60330d2dcb4f7070a2eecc53
박형규 <gudrb4869@naver.com>
2024-02-13 11:14:55 +0900

    style: 코드 수정
    Former-commit-id: afd1e582721f6c041de6a3f30fdd810531661c0c

--- backend/src/main/java/com/ssafy/backend/domain/training/controller/TrainingController.java
+++ backend/src/main/java/com/ssafy/backend/domain/training/controller/TrainingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dbb4b5e4dc87d05402ac5b96ea3fe58fb7eef174369127788c8a00c12777bb6
-size 7984
+oid sha256:66057c57aa239a4fe019ba047095e2cf7af740b1128c7150f9e0972cd2d9d265
+size 7983
