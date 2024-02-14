874f4e525de28db7bb6a1361e2130af41c534e59
김태훈 <xogns5@naver.com>
2024-02-14 10:01:31 +0900

    주석삭제
    Former-commit-id: 7f4bb1a42fb8ff94b66e92e2c55cad7d57c6576a

--- backend/src/main/java/com/ssafy/backend/domain/post/controller/FaqController.java
+++ backend/src/main/java/com/ssafy/backend/domain/post/controller/FaqController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3091d18d06fb9bdafd882ae6782becf2b5d6e93c5025ec33a5ce698fced4e3a
-size 1121
+oid sha256:3b0a9230e74033db7b0794df9c7054520cd7e80e9542f08624712121b080efda
+size 1157
--- backend/src/main/java/com/ssafy/backend/domain/routine/repository/RoutineTrainingListRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/routine/repository/RoutineTrainingListRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b7dace6c05d3b92b1ec267991b503c93efbcecb2bd6aaaa40d86bf5b30fac40
-size 1494
+oid sha256:77ab1416f310eb7b88e04e89afdd6bd4f0cbaf9187377a22de3c55728db8045f
+size 1562
