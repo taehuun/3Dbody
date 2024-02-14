e2e0c054680d1b148c8e28269924b8e99fae1c2a
김민욱 <minwookkim@naver.com>
2024-02-14 19:32:46 +0900

    Feat: 3D모델 수정중
    Former-commit-id: 989a9b0e218a1db7dc9770f247b599305bc3d716

--- frontend/src/components/threedmodel/ThreeD.jsx
+++ frontend/src/components/threedmodel/ThreeD.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f81d7e60244c0b603f4d1a335d4bc2b8354ade4a207d00379ef231d2fbc5a985
-size 2529
+oid sha256:83eb9e4f8e6378afed872bc5b9db574ddf3a355a1ecbeca3b93b9f8108927dd9
+size 3039
