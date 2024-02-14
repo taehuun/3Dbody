e1a6aca2b5b8cfe70923da599fb3869979e66044
이승호 <kials1@naver.com>
2024-02-14 14:07:16 +0900

    Fix: pwa display 수정
    Former-commit-id: daaa4d34eb321d13737cb69525279b3be309aff5

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1ea371f4a4199f5e9d878482fed201dae0647cbe30792c24ede5888d38ed174
-size 2314
+oid sha256:83df616e38672efd83f3cf433f91667a01e53627850089a6ed76c0c75dfb3a7c
+size 2378
