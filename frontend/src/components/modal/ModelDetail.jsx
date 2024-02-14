10be76cddcb37e52000f597e77daa6f036524a95
이승호 <kials1@naver.com>
2024-02-14 15:42:37 +0900

    Feat: 모델 예측 모달 변경
    Former-commit-id: 53333a4262a39133741071c463a30859a6cb5b2a

--- frontend/src/components/modal/ModelDetail.jsx
+++ frontend/src/components/modal/ModelDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af25769b4e1b19e11f684310fc4e71d76335f9aedd0172067227651250db79b6
-size 554
+oid sha256:bb37d8efba3a9b66b36e998a5b0f0655ba6c11934412ea18b832e255fc19c48d
+size 650
--- frontend/src/components/modal/ModelHistoryModal.jsx
+++ frontend/src/components/modal/ModelHistoryModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea9181252447f1f2425eff95c7d84f42736bfb79daf7ff34baf1bd63b2b4e43a
-size 4255
+oid sha256:084c45dd7d3054ea016e24d1e81baf4a6086b9defd28f652ff46b731deb0bb29
+size 4394
