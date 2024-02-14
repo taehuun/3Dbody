28ea081fb8c432724eb7b860f93bd7e75f377d05
박형규 <gudrb4869@naver.com>
2024-02-14 09:20:55 +0900

    style: 로그 삭제
    Former-commit-id: 608edc1ad91fc326ce94a593a1f63428d9ce9c12

--- backend/src/main/java/com/ssafy/backend/domain/training/service/TrainingServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/domain/training/service/TrainingServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bc72b7c70e7e5e8ae4ed2a7df8365ecbbb6f75ff44c65aff68982f4ee8aa705
-size 4265
+oid sha256:5fdf9fa05ebc8f472b6553c0938aadb349b6d4ea0f8a4374fc991341b56f2251
+size 4085
