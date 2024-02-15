9cf97aef258917ed9020e634333971eec8fce0eb
이승호 <kials1@naver.com>
2024-02-15 09:23:34 +0900

    Feat: 모델 상세 모달 인풋 수정
    Former-commit-id: 93e6d8929d8d1e36b5e2797a49314167e05ec74d

--- frontend/src/components/modal/ModelDetailModal.jsx
+++ frontend/src/components/modal/ModelDetailModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e1645a6f70c0405c76816b581e12fdbb288c19be5078436e47824d187e8096f
-size 7987
+oid sha256:ae709c32cc892295bdf433259cf3b61e04132a0f8c3876a3ee6e04ff929faa20
+size 8652
--- frontend/src/components/threedmodel/ThreeD.jsx
+++ frontend/src/components/threedmodel/ThreeD.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:deeeb30948b10633a61c12b03c11b31fe7e4f6726ad8bdc996e38906577d3e69
-size 3337
+oid sha256:b071ee978cd349bdf3c52bcc40cd64e562809287dc7dc2d75e56b180fb7f9c3a
+size 2829
