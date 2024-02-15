a53eb0205fc73357097282db91d6734b4524a3de
이승호 <kials1@naver.com>
2024-02-15 10:36:53 +0900

    Feat: 모델 상세 모달 수정
    Former-commit-id: 129df93c8278502631efe159074ce82078aff16f

--- frontend/src/components/modal/ModelDetail.jsx
+++ frontend/src/components/modal/ModelDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb37d8efba3a9b66b36e998a5b0f0655ba6c11934412ea18b832e255fc19c48d
-size 650
+oid sha256:176d578d2e461d91abd6b830638364d67b2821c1b845eb8e53eb07b5aed9637e
+size 677
