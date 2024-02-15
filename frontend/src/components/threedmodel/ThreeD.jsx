22402d9039ef5e61877b74b445af059ad1201c80
김민욱 <minwookkim@naver.com>
2024-02-15 09:02:04 +0900

    Fix: 3D 렌더링 수정
    Former-commit-id: 1ed10921e63702c9583ad170686c344a52a9c505

--- frontend/src/components/threedmodel/ThreeD.jsx
+++ frontend/src/components/threedmodel/ThreeD.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83eb9e4f8e6378afed872bc5b9db574ddf3a355a1ecbeca3b93b9f8108927dd9
-size 3039
+oid sha256:deeeb30948b10633a61c12b03c11b31fe7e4f6726ad8bdc996e38906577d3e69
+size 3337
